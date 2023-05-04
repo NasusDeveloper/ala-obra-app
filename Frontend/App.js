daf8261a09ea76a4cd37ccca614c69ddc1ecaf85
liessel3@gmail.com <liessel3@gmail.com>
2023-05-03 22:54:39 -0400

    instalando stack, native stack, drawer, bottom tabs

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {};
 import { View, Text } from 'react-native';
 
 export default function App() {
