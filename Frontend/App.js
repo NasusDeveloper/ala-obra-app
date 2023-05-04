9fd39d012ba5717804c63b8dde1372faa9aadc85
Ignacio Quevedo <ignacio.quevedo02@inacapmail.cl>
2023-05-03 23:26:57 -0400

    Importa native navigator

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import Checkbox from 'expo-checkbox'
 import { View, Text } from 'react-native'
+
+import Checkbox from 'expo-checkbox'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 export default function App() {
   return (
