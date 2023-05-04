ebd84d6d06e2d01454f12c240bf7f2a737ebe08a
unknown <josen2530@gmail.com>
2023-05-04 19:19:15 -0400

    registro de usuario en frontend terminado

--- Frontend/app/Screen/auth/RegisterScreen.js
+++ Frontend/app/Screen/auth/RegisterScreen.js
@@ -1,46 +1,62 @@
 import React from "react";
-import { View, Text, SafeAreaView, Keyboard, TouchableWithoutFeedback } from "react-native";
+import { View, Text, SafeAreaView, Keyboard } from "react-native";
 import { useState } from "react";
 import { ScrollView, TextInput, } from "react-native-gesture-handler";
 import { styles, toastConfig } from "../../../style";
 import { Button } from "react-native";
 import { Toast } from "react-native-toast-message/lib/src/Toast";
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import Checkbox from "expo-checkbox";
 import { useNavigation } from "@react-navigation/native";
+import { TouchableWithoutFeedback } from "react-native";
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+
 
 const RegisterScreen = () => {
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [password_confirmation, setPassword_confirmation] = useState("")
+  const [tc, setTc] = useState("false")
 
   const clearTextInput = () => {
+    setName('')
     setEmail('')
     setPassword('')
+    setPassword_confirmation('')
+    setTc('false')
   }
+
+  const navigation = useNavigation()
 
   const handleFormSubmit = () => {
-    if (email && password) {
-      console.log("Inicio de sesion exitoso")
-      const formData = { email, password }
-      console.log(formData)
-      clearTextInput()
-      Toast.show({
-        type: 'done',
-        position: 'top',
-        topOffset: 0,
-        text1: "Inicio de sesion exitoso"
-      })
-
-    } else {
-      console.log("Debe rellenar todos los campos")
-      Toast.show({
-        type: 'warning',
-        position: 'top',
-        topOffset: 0,
-        text1: "Debe rellenar todos los campos"
-      })
-    }
-  }
+    if (name && email && password && password_confirmation && tc) {
+if(password === password_confirmation){
+  console.log("Registro Exitoso")
+  const formData = { name, email, password ,password_confirmation, tc}
+  console.log(formData)
+  clearTextInput()
+  Toast.show({
+    type: 'done',
+    position: 'top',
+    topOffset: 0,
+    text1: "Registro Exitoso"
+  })
+}else{console.log("Las contraseñas no coinciden")
+Toast.show({
+  type: 'warning',
+  position: 'top',
+  topOffset: 0,
+  text1: "Las contraseñas no coinciden"
+})
+}
+} else {console.log("Debe rellenar todos los campos")
+Toast.show({
+  type: 'warning',
+  position: 'top',
+  topOffset: 0,
+  text1: "Debe rellenar todos los campos"
+})
+}}
   return (
     <SafeAreaView>
       <Toast config={toastConfig} />
@@ -49,43 +65,46 @@ const RegisterScreen = () => {
 
         <View style={{ marginHorizontal: 30 }}>
           <View style={[styles.inputWithLabel, { marginBottom: 10, marginTop: 30 }]}>
-
-            <View style={{ alignSelf: 'center', marginBottom: 10 }}>
+          <View style={{ alignSelf: 'center', marginBottom: 10 }}>
               <MaterialIcons name='construction' color='#E8C64A' size={100} />
             </View>
+            <View style={styles.inputWithLabel}>
+              <Text style={styles.labelText}>Nombre:</Text>
+              <TextInput style={styles.input} Value={name} onChangeText={setName} 
+              placeholder="Escribe tu Nombre" onPress={console.log(name)} />
+            </View>
 
-            <Text style={styles.labelText}>Correo:</Text>
-            <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder="Ingresa tu Correo electronico"
-              onPress={console.log(email)} KeyboardType='email-address' />
+            <View style={[styles.inputWithLabel, { marginBottom: 10 }]}>
+              <Text style={styles.labelText}>Email: </Text>
+              <TextInput style={styles.input} Value={email} placeholder="Ingresa tu Correo electronico"
+                onChangeText={setEmail} onPress={console.log(email)} KeyboardType='email-address'></TextInput>
+            </View>
 
-          </View>
+            <View style={[styles.inputWithLabel, { marginBottom: 10 }]}>
+              <Text style={styles.labelText}>Contraseña: </Text>
+              <TextInput style={styles.input} Value={password} placeholder="Ingresa tu Contraseña"
+                onChangeText={setPassword} onPress={console.log(password)} secureTextEntry={true}></TextInput>
+            </View>
 
-          <View style={styles.inputWithLabel}>
+            <View style={[styles.inputWithLabel ,{ marginBottom: 10 }]}>
+              <Text style={styles.labelText}>Confirma la Contraseña </Text>
+              <TextInput style={styles.input} Value={password_confirmation} placeholder="Ingresa tu Contraseña"
+                onChangeText={setPassword_confirmation} onPress={console.log(password_confirmation)} secureTextEntry={true}></TextInput>
+            </View>
 
-            <Text style={styles.labelText}>Contraseña:</Text>
-            <TextInput style={styles.input} value={password} onChangeText={setPassword} placeholder="Ingresa tu Contraseña"
-              onPress={console.log(password)} secureTextEntry={true} />
+            <View style={{ flex: 1, flexDirection: 'row' , marginTop: 10}}>
+              <Checkbox value={tc} onValueChange={setTc} color={tc ? '#4630EB' : undefined} />
+              <Text style={styles.labelText}>He leído y acepto los términos y condiciones </Text>
+            </View>
 
-          </View>
-          <View style={{ width: 200, alignSelf: 'center', margin: 20 }}>
-            <Button title='Entrar' onPress={handleFormSubmit} color='purple' />
-          </View>
+            <View style={{ width: 200, alignSelf: 'center', margin: 20 }}>
+              <Button title='Aceptar' onPress={handleFormSubmit} color='purple' />
+            </View>
 
-          <View style={{ flexDirection: 'row' }}>
-            <View style={{ flex: 1 }}>
-
-              <TouchableWithoutFeedback onPress={() => { console.log('Password Reset Screen') }}>
-                <Text style={{ fontWeight: 'bold' }}>Has olvidado tu contraseña?</Text>
+            <View style={{ alignItems: 'flex-end' }}>
+              <TouchableWithoutFeedback onPress={()=>{navigation.navigate('UserLogin')}}>
+                <Text style={{ fontWeight:'bold'}}>ya se ha registrado? inicie sesion</Text>
               </TouchableWithoutFeedback>
-
-            </View >
-
-            <View style={{ flex: 1 }}>
-
-              <TouchableWithoutFeedback onPress={() => { console.log('New Register Screen') }}>
-                <Text style={{ fontWeight: 'bold' }}>Usuario nuevo? Regese</Text>
-              </TouchableWithoutFeedback>
-
             </View>
           </View>
         </View>
