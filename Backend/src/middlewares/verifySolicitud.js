cf71251967ad439c9999d86e8d2c9f4467010fc3
Ignacio Quevedo <ignacio.quevedo02@inacapmail.cl>
2023-06-02 00:50:26 -0400

    Se intenta arreglar el error 404 de solicitud, pero sigue sin funcionar

--- Backend/src/middlewares/verifySolicitud.js
+++ Backend/src/middlewares/verifySolicitud.js
@@ -1,11 +1,11 @@
-import jwt from "jsonwebtoken";
-import config from "../config.js";
+import jwt from 'jsonwebtoken';
+import config from '../config.js';
 
-export const verifyToken = (req, res, next) => {
+export const verifySolicitud = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ msg: "No se proporcionó un token de autenticación" });
+    return res.status(401).json({ msg: 'No se proporcionó un token de autenticación' });
   }
 
   try {
@@ -14,6 +14,6 @@ export const verifyToken = (req, res, next) => {
     next();
   } catch (error) {
     console.error(error);
-    res.status(401).json({ msg: "Token inválido" });
+    res.status(401).json({ msg: 'Token inválido' });
   }
 };
--- Frontend/app/Screen/auth/AutoStack.js
+++ Frontend/app/Screen/auth/AutoStack.js
@@ -46,4 +46,3 @@ const AutoStack = () =>{
 )}
 
 export default AutoStack
-
